cb9995d516f91306fa435d5b47c9847f3db3dcd8
Maxime Valleron <mvalleron@emi.u-bordeaux1.fr>
2016-12-05 17:46:00 +0100

    Modification du Makefile pour créer tempo.o, Ajout de tempo.c, Mise à jour de libgame.a, Modification de timer.h

--- Projet/include/timer.h
+++ Projet/include/timer.h
@@ -2,12 +2,9 @@
 #ifndef TIMER_IS_DEF
 #define TIMER_IS_DEF
 
-// Tyoe d'événements "utilisateur"
-enum {
-  TIMER_ONCE,
-  TIMER_REPEAT
-};
+int timer_init (void);
+void timer_set (Uint32 delay, void *param);
 
-SDL_TimerID timer_set (Uint32 delay, int timer_type, void *param);
+void sdl_push_event (void *param);
 
 #endif
---
+++ Projet/src/tempo.c
@@ -0,0 +1,60 @@
+#include <SDL.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <sys/time.h>
+#include <signal.h>
+#include <pthread.h>
+
+#include "timer.h"
+
+void traitant(int s){
+  printf("signal %d\n", s);
+}
+
+void f(){
+  struct sigaction s;
+  sigset_t mask,empty_mask;
+  sigemptyset(&mask);
+  sigemptyset(&empty_mask);
+  s.sa_handler = traitant;
+  sigemptyset(&s.sa_mask);
+  s.sa_flags=0;
+  sigaction(SIGINT,&s,NULL);
+  printf("ok\n");
+  while(1){
+    sigsuspend(&empty_mask);
+  }
+}
+
+// Return number of elapsed µsec since... a long time ago
+static unsigned long get_time (void)
+{
+  struct timeval tv;
+
+  gettimeofday (&tv ,NULL);
+
+  // Only count seconds since beginning of 2016 (not jan 1st, 1970)
+  tv.tv_sec -= 3600UL * 24 * 365 * 46;
+  
+  return tv.tv_sec * 1000000UL + tv.tv_usec;
+}
+
+#ifdef PADAWAN
+
+// timer_init returns 1 if timers are fully implemented, 0 otherwise
+int timer_init (void)
+{
+  printf("ok\n");
+  pthread_t pid;
+  pthread_create(&pid,NULL,f,NULL);
+  return 0; // Implementation not ready
+}
+
+void timer_set (Uint32 delay, void *param)
+{
+  // TODO
+}
+
+#endif
