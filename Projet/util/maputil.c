b1cf3c957aa249c1e2e750b3585104cd565f125b
Gautier DELACOUR <gautier.delacour@etu.u-bordeaux.fr>
2016-12-05 16:59:52 +0100

    ajout de la regle de compilation de maputil.c dans Makefile

---
+++ Projet/util/maputil.c
@@ -0,0 +1,13 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "map.h"
+#include "error.h"
+
+int main()
+{
+  return EXIT_SUCCESS;
+}
